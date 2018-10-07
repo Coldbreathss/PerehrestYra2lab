b620134d1308851462c39cae61b36694102fba62
Coldbreathss <Coldbreathss@mail.ru>
2018-10-07 20:25:02 +0300

    ver2

--- lectures 2/2.1/methodChiper.js
+++ lectures 2/2.1/methodChiper.js
@@ -12,8 +12,6 @@ function encrypt() {
             kod = parseInt(-23);
         } else if (code == parseInt(120) || code == parseInt(121) || code == parseInt(122)) {
             kod = parseInt(-23);
-        } else {
-            kod = parseInt(3);
         };
         code = code + kod;
         out += String.fromCharCode(code);
@@ -28,16 +26,16 @@ function decrypt() {
     kod = parseInt(kod);
 
        let out = '';
-    for (let i=0; i< text.length; i++){
-        let code = text.charCodeAt(i);
-        if (code == parseInt(88) || code == parseInt(89) || code == parseInt(90)) {
-            kod = parseInt(-23);
-        } else if (code == parseInt(120) || code == parseInt(121) || code == parseInt(122)) {
-            kod = parseInt(-23);
+    for (let i = 0; i < text.length; i++) {
+        code = text.charCodeAt(i);
+        if (code == parseInt(65) || code == parseInt(66) || code == parseInt(67)) {
+            kod = parseInt(23);
+        } else if (code == parseInt(97) || code == parseInt(98) || code == parseInt(99)) {
+            kod = parseInt(23);
         } else {
-            kod = parseInt(3);
+            kod = parseInt(-3);
         };
-        code = code - kod;
+        code += kod
         out += String.fromCharCode(code);
     }
     document.getElementById('encrypted').innerHTML = out;
