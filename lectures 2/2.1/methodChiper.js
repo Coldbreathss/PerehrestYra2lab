5b7a6d53dc55e38c900a08d0177d1aec6787715b
Coldbreathss <Coldbreathss@mail.ru>
2018-10-08 13:58:48 +0300

    2.2

--- lectures 2/2.1/methodChiper.js
+++ lectures 2/2.1/methodChiper.js
@@ -28,14 +28,12 @@ function decrypt() {
        let out = '';
     for (let i = 0; i < text.length; i++) {
         code = text.charCodeAt(i);
-        if (code == parseInt(65) || code == parseInt(66) || code == parseInt(67)) {
-            kod = parseInt(23);
-        } else if (code == parseInt(97) || code == parseInt(98) || code == parseInt(99)) {
-            kod = parseInt(23);
-        } else {
-            kod = parseInt(-3);
+        if (code == parseInt(88) || code == parseInt(89) || code == parseInt(90)) {
+            kod = parseInt(-23);
+        } else if (code == parseInt(120) || code == parseInt(121) || code == parseInt(122)) {
+            kod = parseInt(-23);
         };
-        code += kod
+        code = code - kod;
         out += String.fromCharCode(code);
     }
     document.getElementById('encrypted').innerHTML = out;
