fa72ee7bdd3ab82d6818268d06b784f317c42438
Coldbreathss <Coldbreathss@mail.ru>
2018-10-20 05:08:42 +0300

    Add files via upload

--- lectures 2/2.4/formautor.js
+++ lectures 2/2.4/formautor.js
@@ -2,7 +2,7 @@ function valid(form) {
 	var fail = false;
 	var email = form.email.value;
 	var password = form.password.value;
-	var adr_pattern = /[0-9a-z_-]+@[0-9a-z_-]+\.[a-z]{2,5}/i;
+	var adr_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 	var adr_password = /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/;
 
 	if (adr_pattern.test(email) == false)
